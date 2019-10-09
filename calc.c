61d0dc0c8d86bf6695cd2ad8a91dfef6430846bf
naoaki23 <naomi.akiyama@cooper.edu>
2019-10-09 19:02:48 -0400

    NaomiCalc2

--- calc.c
+++ calc.c
@@ -6,14 +6,12 @@ int main(int argc, char **argv)
 	double num1 = atof(argv[2]);
 	double num2 = atof(argv[3]);
 
-    if(argv[1][0] == "+") {
-    	printf("%lf\n", num1 + num2);
-    } else if(argv[1][0] == "-") {
-    	printf("%lf\n", num1 - num2);
-    } else if(argv[1][0] == "x") {
-    	printf("%lf\n", num1 * num2);
-    } else if(argv[1][0] == "/") {
-    	printf("%lf\n", num1 / num2);
-    }
-	return 0;
+    if(argv[1][0] == '+')
+    	printf("%f\n", num1 + num2);
+    else if(argv[1][0] == '-')
+    	printf("%f\n", num1 - num2);
+    else if(argv[1][0] == 'x')
+    	printf("%f\n", num1 * num2);
+    else if(argv[1][0] == '/')
+    	printf("%f\n", num1 / num2);
 }
